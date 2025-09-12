4bb1d98ed2236dd835b044144e86afb551da233b
Logan Williams <loftylogan@gmail.com>
2025-09-12 14:57:26 -0500

    Cleanup cont.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use clap::{Parser, Subcommand, ArgGroup};
 use halcyon_lib::*;
 use serde::Deserialize;
-use colored::Colorize;
+use colored::{ColoredString, Colorize};
 use std::time::{Instant};
 
+//config file struct
 #[derive(Deserialize)]
 struct Config {
    infiles: Vec<String>,
    outfile: String,
 }
 
+//---ARGS---
 #[derive(Debug, Parser)]
 #[command(group(
     ArgGroup::new("build_source")
@@ -48,6 +50,7 @@ struct CmdArgs {
     command: Commands,
 }
 
+//---WASM---
 pub fn execute(wasm: Vec<u8>) {
     use wasmtime::*;
     let mut config = Config::default();
@@ -75,16 +78,28 @@ pub fn execute(wasm: Vec<u8>) {
     let _instance = linker.instantiate(&mut store, &module).unwrap();
 }
 
+//---CLI_HELPERS---
 fn check_valid(file_name : &String) -> std::result::Result<(), colored::ColoredString> {
     println!("Checking {}...", file_name.blue());
     let bytes = file_name.as_bytes();
+    let mut errors : Vec<ColoredString> = vec![];
     if bytes[(bytes.len() - 3)..] != [b'.',b'h',b'c'] 
     {
-        return std::result::Result::Err("Format Error: File must be a Halcyon source file! (.hc)".red());
+        errors.push("Format Error: Input files must be Halcyon source files! (.hc)".truecolor(255,204,204));
     }
     let file_as_string = std::fs::read_to_string(std::path::PathBuf::from(file_name)).map_err(|e| e.to_string())?;
     compile(&file_as_string)?;
-    println!("{} {} checks", &file_name," passed".green());
+    match errors.len() {
+        0 => println!("{} {} checks",file_name.blue(), "passed".green()),
+        _ => 
+        {
+            println!("{} {} {} {}",file_name.blue(), "failed".red(), errors.len().to_string().red(), if errors.len() > 1 {"checks"} else {"check"});
+            for error in errors {
+                println!("{}", error);
+            }
+            return std::result::Result::Err("One or more input files didn't pass checks".red())
+        }
+    };
     Ok(())
 
 } 
@@ -108,6 +123,7 @@ fn build (in_file_names : Vec<String>, out_file_name : &String, with_binary : bo
     Ok(binary)
 }
 
+//---CLI---
 fn hcc_main() -> std::result::Result<(), colored::ColoredString> {
     let args = CmdArgs::parse();
     match args.command {
@@ -148,11 +164,13 @@ fn hcc_main() -> std::result::Result<(), colored::ColoredString> {
                     let cfgfile = std::fs::read_to_string(std::path::PathBuf::from(cfg)).map_err(|e| e.to_string())?;
                     let config: Config = toml::from_str(&cfgfile).unwrap();
                     let binary = build(config.infiles.to_vec(), &config.outfile, false)?;
+                    println!("Running...\n--------------------------------");
                     execute(binary);
                     
                 }
                 (None, Some(inp)) => {
                     let binary = build([inp].to_vec(), &String::from("Spink"), false)?;
+                    println!("Running...\n--------------------------------");
                     execute(binary);
                 }
                 _ => unreachable!("Clap enforces mutual exclusion"),
